5fff7c665e5416c9137f8cf7f63c6999d547d6e8
oxangyal <Oxana.mitch@gmail.com>
2024-03-08 14:59:39 -0500

    Build basic navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,43 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import { getAllData } from './util/index';
 
-const URL = 'http://localhost:8000/api/v1/';
+import { Route, Routes } from 'react-router-dom';
+
+import Layout from './components/Layout';
+
+// import { useEffect, useState } from 'react';
+
+// import { getAllData } from './util/index';
+
+// const URL = 'http://localhost:8000/api/v1/';
 
 const App = () => {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    (async () => {
-      const myData = await getAllData(URL);
-      setMessage(myData.data);
-    })();
-
-    return () => {
-      console.log('unmounting');
-    };
-  }, []);
-
   return (
     <>
-      <h1>{message}</h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* <Route index element={<HomePage />} /> */}
+          {/* <Route path="/login" element={<LoginForm />} /> */}
+          {/* <Route path="*" element={<NotFoundPage />} /> */}
+        </Route>
+      </Routes>
     </>
   );
+
+  // const [message, setMessage] = useState('');
+  // useEffect(() => {
+  //   (async () => {
+  //     const myData = await getAllData(URL);
+  //     setMessage(myData.data);
+  //   })();
+  //   return () => {
+  //     console.log('unmounting');
+  //   };
+  // }, []);
+  // return (
+  //   <>
+  //     <h1>{message}</h1>
+  //   </>
+  // );
 };
 
 export default App;
