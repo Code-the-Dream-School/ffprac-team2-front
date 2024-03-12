ae1362c6de82c4d3e4b56357e1c098cd86eea791
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2024-03-12 18:55:11 -0400

    :bug: fix incorrect formatting that snuck in

--- src/theme.ts
+++ src/theme.ts
@@ -2,13 +2,13 @@ import { extendTheme } from '@chakra-ui/react';
 
 // example theme
 export const theme = extendTheme({
-  styles: {
-    global: {
-      // styles for the `body`
-      body: {
-        bg: '#E7E0D6',
-        color: '#000000',
-      },
+    styles: {
+        global: {
+            // styles for the `body`
+            body: {
+                bg: '#E7E0D6',
+                color: '#000000',
+            },
+        },
     },
-  },
 });
