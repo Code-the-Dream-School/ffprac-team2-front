1042cc3e66beb0e9b1c7158d6dfd059e46268a7a
Kremifany <34751960+Kremifany@users.noreply.github.com>
2024-03-14 17:54:51 -0400

    Interfaces updated

--- src/models/interfaces.ts
+++ src/models/interfaces.ts
@@ -4,7 +4,20 @@ interface StudentTutorInfo {
     availability: string;
     tutorName: string;
 }
+export interface User {
+    user_id: string; // Auto-generated
+    firstName: string; // First name with required validation
+    lastName: string; // Last name with required validation
+    email: string; // Email with required validation
+    password: string; // Password with required validation
+    role: UserRole; // Role using a UserRole enum
+}
 
+export enum UserRole {
+    admin,
+    tutor,
+    parent,
+}
 export interface Student {
     name: string;
     grade: string;
@@ -73,17 +86,22 @@ export interface Subject {
     _id: string; // Subject ID from the database
     name: string; // Subject name
 }
-export interface Tutor {
+export interface Tutor extends User {
     _id: string;
     user: string; // Reference to the User document
     subjects: Subject[]; // Array of Subject objects
     grades: string[]; // Array of grade levels (e.g., ['K', '1', '2', ...])
-    aboutSummary: string; // Summary about the tutor (max 150 characters)
+    about: string; // Summary about the tutor (max 150 characters)
     education?: string; // Optional education details
     students?: string[]; // Array of student user IDs (optional)
     image?: string; // Optional image URL for the tutor
     availability?: string[]; // Array of availability objects (optional)
+    expirience?: string; //Optional expirience
 }
+export interface AuthPageProps {
+    activeTab: 'register' | 'login';
+}
+
 export interface AuthPageProps {
     activeTab: 'register' | 'login';
 }
