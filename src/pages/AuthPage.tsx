7efc2074fdfe7eb95ac2eeeb8e61845b57518dfb
Alena Dudko <e.v.dudko@gmail.com>
2024-03-16 14:45:19 -0500

    Work in progress. Set the defaultIndex based on activeTab prop

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -13,48 +13,49 @@ import RegistrationForm from '../components/RegistrationForm';
 import LoginForm from '../components/LoginForm';
 
 
-const AuthPage: React.FC = () => {
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        // Some logic will come here.
-    };
+interface AuthPageProps {
+    activeTab: 'register' | 'login';
+}
 
-    return (
-        <Flex align="center" justify="center">
-            <Box>
-                <Tabs variant="unstyled" defaultIndex={1}>
-                    <TabList
-                        bg="#D9D9D9" // Set background color to #D9D9D9
-                        width="350px"
-                        height="60px"
-                        borderRadius="20px"
-                        display="flex"
-                        justifyContent="center"
-                        margin="0 15px"
-                    >
-                        <Tab _selected={{ backgroundColor: 'white' }} flex="1" borderRadius="20px" textAlign="center">
-                            Register
-                        </Tab>
-                        <Tab _selected={{ backgroundColor: 'white' }} flex="1" borderRadius="20px" textAlign="center">
-                            Login
-                        </Tab>
-                    </TabList>
-                    <TabPanels>
-                        <TabPanel>
-                            <Flex align="center" justify="center" direction="column">
-                                <RegistrationForm />
-                            </Flex>
-                        </TabPanel>
-                        <TabPanel>
-                            <Flex align="center" justify="center" direction="column">
-                                <LoginForm />
-                            </Flex>
-                        </TabPanel>
-                    </TabPanels>
-                </Tabs>
-            </Box>
-        </Flex >
-    );
+const AuthPage: React.FC<AuthPageProps> = ({ activeTab }) => {
+    const defaultIndex = activeTab === 'register' ? 0 : 1; // Set the defaultIndex based on activeTab prop
+
+return (
+    <Flex align="center" justify="center">
+        <Box>
+            <Tabs variant="unstyled" defaultIndex={defaultIndex}>
+                <TabList
+                    bg="#D9D9D9" // Set background color to #D9D9D9
+                    width="350px"
+                    height="60px"
+                    borderRadius="20px"
+                    display="flex"
+                    justifyContent="center"
+                    margin="0 15px"
+                >
+                    <Tab _selected={{ backgroundColor: 'white' }} flex="1" borderRadius="20px" textAlign="center">
+                        Register
+                    </Tab>
+                    <Tab _selected={{ backgroundColor: 'white' }} flex="1" borderRadius="20px" textAlign="center">
+                        Login
+                    </Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel>
+                        <Flex align="center" justify="center" direction="column">
+                            <RegistrationForm />
+                        </Flex>
+                    </TabPanel>
+                    <TabPanel>
+                        <Flex align="center" justify="center" direction="column">
+                            <LoginForm />
+                        </Flex>
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+        </Box>
+    </Flex >
+);
 };
 
 export default AuthPage;
