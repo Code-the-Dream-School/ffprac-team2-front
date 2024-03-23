8f91b5307838b878bdd0f983c17708eab5bfe1e3
mynewyear <natalia.iakovleva@hotmail.com>
2024-03-23 15:07:20 -0700

    theme was updated

--- src/util/theme.ts
+++ src/util/theme.ts
@@ -1,5 +1,59 @@
 import { extendTheme } from '@chakra-ui/react';
 
+export const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: '#E7E0D6',
+        color: '#000000',
+        fontSize: '11px', 
+      },
+    },
+  },
+  colors: {
+    customWhite: '#FFFFFF', 
+  },
+  components: {
+    Button: {
+      baseStyle: {
+        fontWeight: 'normal', 
+      },
+      sizes: {
+         // landingButtons size as a custom size for buttons
+        landing: {
+            h: '75px', 
+            minW: '350px', 
+            fontSize: '24px',
+          },
+        // dashboardButtons size as a custom size for buttons
+        dashboard: {
+          h: '35px', 
+          fontSize: '12px', 
+          fontWeight: '700', 
+        },
+      },
+      variants: {
+        // button variants. (dashboardButtons styles as variants)
+        buttonYellow: {
+          bg: '#F4CD76',
+          _hover: {
+            bg: 'grey.400',
+          },
+        },
+        buttonTeal: {
+          bg: '#59D3C8',
+          _hover: {
+            bg: 'grey.400',
+          },
+        },
+      },
+    },
+  },
+});
+
+/*
+import { extendTheme } from '@chakra-ui/react';
+
 // example theme
 export const theme = extendTheme({
     styles: {
@@ -34,7 +88,7 @@ export const theme = extendTheme({
         fontWeight: '700',
     },
 });
-
+*/
 // Default braikpoints for informational purposes
 // const breakpoints = {
 //     base: "0em", // 0px
