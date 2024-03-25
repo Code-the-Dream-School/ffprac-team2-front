ddb1e441fbc9bdf9068bc4fe5d816db3e14259ae
Kremifany <34751960+Kremifany@users.noreply.github.com>
2024-03-25 16:17:33 -0400

    TutorProfilePage initial commit, responsive

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import { Route, Routes } from 'react-router-dom';
 
 import Layout from './components/Layout';
 import ParentDashboardPage from './pages/ParentDashboardPage';
+import TutorProfilePage from './pages/TutorProfilePage';
 
 // import { useEffect, useState } from 'react';
 
 // import { getAllData } from './util/index';
 
 // const URL = 'http://localhost:8000/api/v1/';
+
+//after login page tutorProfileForm is called if tutor radion button is checked
 
 const App = () => {
     return (
@@ -19,6 +22,7 @@ const App = () => {
                     {/* <Route index element={<HomePage />} /> */}
                     {/* <Route path="/login" element={<LoginForm />} /> */}
                     <Route path="/parent-dashboard" element={<ParentDashboardPage />} />
+                    <Route path="/tutor-profile" element={<TutorProfilePage />} />
                     {/* <Route path="*" element={<NotFoundPage />} /> */}
                 </Route>
             </Routes>
