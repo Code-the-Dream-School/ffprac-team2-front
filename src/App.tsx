3366dfa2647d5e3b6d2487ccfa75afe80087ead1
mynewyear <natalia.iakovleva@hotmail.com>
2024-03-25 01:00:43 -0700

    check

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,14 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import LandingPage from './pages/LandingPage';
 import ParentDashboardPage from './pages/ParentDashboardPage';
-
-<<<<<<< HEAD
-<<<<<<< HEAD
 import TutorSearchPage from './pages/TutorSearchPage';
-=======
->>>>>>> updated bg, added redirect to login page
-=======
->>>>>>> 7a1811afcdacf6268f80cab94eddd5bef54b523a
+
 import AuthPage from './pages/AuthPage';
 
 // import TutorCard from './components/TutorCard';
 
 // import TutorSearchBar from './components/TutorSearchBar';
 // import TutorSearchPage from './pages/TutorSearchPage';
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-import LandingPage from './pages/LandingPage'
-
-=======
->>>>>>> updated bg, added redirect to login page
-=======
->>>>>>> 7a1811afcdacf6268f80cab94eddd5bef54b523a
 
 // import { useEffect, useState } from 'react';
 
@@ -40,18 +25,10 @@ const App = () => {
         <>
             <Routes>
                 <Route path="/" element={<Layout />}>
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                     {/* <Route index element={<HomePage />} /> */}
-                    <Route path="/auth" element={<AuthPage activeTab={'login'} />} />
-=======
->>>>>>> updated bg, added redirect to login page
                     <Route index element={<LandingPage parentButtonText="I'm a parent" tutorButtonText="I'm a tutor" />} />
                     <Route path="/auth" element={<AuthPage activeTab={'login'} />} />
-=======
-                    <Route index element={<LandingPage parentButtonText="I'm a parent" tutorButtonText="I'm a tutor" />} />
-                    <Route path="/auth" element={<AuthPage activeTab={'login'} />} />
->>>>>>> 7a1811afcdacf6268f80cab94eddd5bef54b523a
                     {/* <Route path="/login" element={<LoginForm />} /> */}
                     <Route path="/parent-dashboard" element={<ParentDashboardPage />} />
                     {/* <Route path="/tutorcard" element={<TutorCard tutor={undefined} />} /> */}
