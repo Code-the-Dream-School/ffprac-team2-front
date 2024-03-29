ac02282f12eea4a51cd7a78dd2315c1cad80fe04
Alena Dudko <e.v.dudko@gmail.com>
2024-03-29 16:46:36 -0500

    Fix UI defects.

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Stack, FormControl, FormLabel, Input, Radio, RadioGroup, Button, Box, Spinner, FormErrorMessage, Tooltip, useBreakpointValue } from '@chakra-ui/react';
+import {
+    Stack,
+    FormControl,
+    FormLabel,
+    Input,
+    Radio,
+    RadioGroup,
+    Button,
+    Box,
+    Spinner,
+    FormErrorMessage,
+    Tooltip,
+    useBreakpointValue,
+} from '@chakra-ui/react';
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 import { RegistrationFormData } from '../models/interfaces';
-
-
-
 
 const labelStyle = {
     fontSize: '14px',
@@ -29,17 +39,16 @@ const RegistrationForm: React.FC = () => {
     const fieldHeight = useBreakpointValue({ base: '40px', md: '50px' });
     const inputStyle = {
         width: fieldLength,
-        height: fieldHeight, 
+        height: fieldHeight,
         backgroundColor: 'white',
     };
-    
+
     const buttonLength = useBreakpointValue({ base: '120px', md: '150px' });
     const buttonHeight = useBreakpointValue({ base: '40px', md: '50px' });
     const buttonStyle = {
         width: buttonLength,
         height: buttonHeight,
-
-    }
+    };
 
     const initialValues: RegistrationFormData = {
         firstName: '',
@@ -76,21 +85,22 @@ const RegistrationForm: React.FC = () => {
                     .oneOf([Yup.ref('password'), null], 'Passwords must match')
                     .required('Confirmation of password is required'),
                 role: Yup.string().required('Please select a role'),
-
             })}
-
             onSubmit={async (values, { setSubmitting, setStatus, setFieldError }) => {
-                setStatus('Processing')
+                setStatus('Processing');
                 try {
                     const requestData = {
                         firstName: values.firstName,
                         lastName: values.lastName,
                         email: values.email,
                         password: values.password,
-                        role: values.role
+                        role: values.role,
                     };
                     //console.log('Request Data:', requestData);
-                    const response = await axios.post("https://ffprac-team2-back.onrender.com/api/v1/auth/register", requestData);
+                    const response = await axios.post(
+                        'https://ffprac-team2-back.onrender.com/api/v1/auth/register',
+                        requestData
+                    );
                     const { firstName, lastName, email, role, token } = response.data.user;
                     localStorage.setItem('token', token);
                     const userData = { firstName, lastName, email, role };
@@ -107,9 +117,12 @@ const RegistrationForm: React.FC = () => {
             }}
         >
             {(formik) => (
-                <Form onSubmit={formik.handleSubmit} >
-                    <Stack spacing={4} style={{width: fieldLength, height: fieldHeight}} >
-                        <FormControl isRequired isInvalid={!!(formik.errors.firstName && formik.touched.firstName)}>
+                <Form onSubmit={formik.handleSubmit}>
+                    <Stack spacing={4} style={{ width: fieldLength }}>
+                        <FormControl
+                            isRequired
+                            isInvalid={!!(formik.errors.firstName && formik.touched.firstName)}
+                        >
                             <FormLabel htmlFor="firstName" style={labelStyle}>
                                 First Name
                             </FormLabel>
@@ -120,11 +133,14 @@ const RegistrationForm: React.FC = () => {
                                 name="firstName"
                                 style={inputStyle}
                             />
-                            {(formik.errors.firstName && formik.touched.firstName) && (
+                            {formik.errors.firstName && formik.touched.firstName && (
                                 <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
                             )}
                         </FormControl>
-                        <FormControl isRequired isInvalid={!!(formik.errors.lastName && formik.touched.lastName)}>
+                        <FormControl
+                            isRequired
+                            isInvalid={!!(formik.errors.lastName && formik.touched.lastName)}
+                        >
                             <FormLabel htmlFor="lastName" style={labelStyle}>
                                 Last Name
                             </FormLabel>
@@ -135,11 +151,14 @@ const RegistrationForm: React.FC = () => {
                                 name="lastName"
                                 style={inputStyle}
                             />
-                            {(formik.errors.lastName && formik.touched.lastName) && (
+                            {formik.errors.lastName && formik.touched.lastName && (
                                 <FormErrorMessage>{formik.errors.lastName}</FormErrorMessage>
                             )}
                         </FormControl>
-                        <FormControl isRequired isInvalid={!!(formik.errors.email && formik.touched.email)}>
+                        <FormControl
+                            isRequired
+                            isInvalid={!!(formik.errors.email && formik.touched.email)}
+                        >
                             <FormLabel htmlFor="email" style={labelStyle}>
                                 Email
                             </FormLabel>
@@ -150,11 +169,14 @@ const RegistrationForm: React.FC = () => {
                                 name="email"
                                 style={inputStyle}
                             />
-                            {(formik.errors.email && formik.touched.email) && (
-                                <FormErrorMessage >{formik.errors.email}</FormErrorMessage>
+                            {formik.errors.email && formik.touched.email && (
+                                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
                             )}
                         </FormControl>
-                        <FormControl isRequired isInvalid={!!(formik.errors.password && formik.touched.password)}>
+                        <FormControl
+                            isRequired
+                            isInvalid={!!(formik.errors.password && formik.touched.password)}
+                        >
                             <FormLabel htmlFor="password" style={labelStyle}>
                                 Password
                             </FormLabel>
@@ -165,11 +187,16 @@ const RegistrationForm: React.FC = () => {
                                 name="password"
                                 style={inputStyle}
                             />
-                            {(formik.errors.password && formik.touched.password) && (
+                            {formik.errors.password && formik.touched.password && (
                                 <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
                             )}
                         </FormControl>
-                        <FormControl isRequired isInvalid={!!(formik.errors.confirmPassword && formik.touched.confirmPassword)}>
+                        <FormControl
+                            isRequired
+                            isInvalid={
+                                !!(formik.errors.confirmPassword && formik.touched.confirmPassword)
+                            }
+                        >
                             <FormLabel htmlFor="confirmPassword" style={labelStyle}>
                                 Confirm Password
                             </FormLabel>
@@ -180,11 +207,17 @@ const RegistrationForm: React.FC = () => {
                                 name="confirmPassword"
                                 style={inputStyle}
                             />
-                            {(formik.errors.confirmPassword && formik.touched.confirmPassword) && (
+                            {formik.errors.confirmPassword && formik.touched.confirmPassword && (
                                 <FormErrorMessage>{formik.errors.confirmPassword}</FormErrorMessage>
                             )}
                         </FormControl>
-                        <RadioGroup id="role" name="role" width="350px" value={formik.values.role} onChange={(value) => formik.setFieldValue('role', value)}>
+                        <RadioGroup
+                            id="role"
+                            name="role"
+                            width="350px"
+                            value={formik.values.role}
+                            onChange={(value) => formik.setFieldValue('role', value)}
+                        >
                             <Stack direction="row" display="flex" alignItems="center">
                                 <Radio value="parent" flex="1" bg="white" size="md" marginLeft={2}>
                                     <span style={radioLabelStyle}>Parent</span>
@@ -194,18 +227,29 @@ const RegistrationForm: React.FC = () => {
                                 </Radio>
                             </Stack>
                         </RadioGroup>
-                        <Stack direction="row" spacing={4} display="flex" alignItems="center" marginTop={4} marginBottom={4}>
-
+                        <Stack
+                            direction="row"
+                            spacing={4}
+                            display="flex"
+                            alignItems="center"
+                            marginTop={4}
+                            marginBottom={4}
+                        >
                             <Tooltip
                                 label="Please fill out all fields and choose 'Parent' or 'Tutor' to complete registration."
                                 openDelay={300}
                                 hasArrow
                                 isDisabled={formik.dirty && formik.isValid}
-
-                                {...tooltipStyle}>
+                                {...tooltipStyle}
+                            >
                                 <Button
                                     type="submit"
-                                    style={{ ...buttonStyle, width: buttonLength, backgroundColor: '#F4CD76', color: 'black' }}
+                                    style={{
+                                        ...buttonStyle,
+                                        width: buttonLength,
+                                        backgroundColor: '#F4CD76',
+                                        color: 'black',
+                                    }}
                                     flex="1"
                                     isDisabled={!formik.dirty || !formik.isValid}
                                 >
@@ -215,7 +259,11 @@ const RegistrationForm: React.FC = () => {
 
                             <Button
                                 type="button"
-                                style={{ ...buttonStyle, backgroundColor: '#59D3C8', color: 'black' }}
+                                style={{
+                                    ...buttonStyle,
+                                    backgroundColor: '#59D3C8',
+                                    color: 'black',
+                                }}
                                 flex="1"
                                 onClick={() => navigate('/')}
                             >
@@ -234,6 +282,4 @@ const RegistrationForm: React.FC = () => {
     );
 };
 
-
 export default RegistrationForm;
-
