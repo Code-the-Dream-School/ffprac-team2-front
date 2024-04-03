fbfdf58424a38cf23e7dbece83062aa02720e982
Kremifany <34751960+Kremifany@users.noreply.github.com>
2024-04-02 23:26:44 -0400

    added multiselect component theme

--- src/util/theme.ts
+++ src/util/theme.ts
@@ -1,4 +1,5 @@
 import { extendTheme } from '@chakra-ui/react';
+import { MultiSelectTheme } from 'chakra-multiselect';
 
 // example theme
 export const theme = extendTheme({
@@ -61,6 +62,23 @@ export const theme = extendTheme({
                         bg: 'grey.400',
                     },
                 },
+            },
+        },
+        MultiSelect: {
+            ...MultiSelectTheme,
+            baseStyle: (props: Record<string, number>) => {
+                const baseStyle = MultiSelectTheme.baseStyle(props);
+                baseStyle.input.bgColor = theme.colors.customWhite;
+                baseStyle.control.minW = 500;
+                baseStyle.button.colorscheme = 'black.400';
+                baseStyle.list.boxShadow = '2xl';
+                baseStyle.selectedItem.colorscheme = 'green';
+                baseStyle.input.outline = 1;
+                baseStyle.selectedItem.colorscheme = 'pink.400';
+                baseStyle.list.color = 'black.100';
+                return {
+                    ...baseStyle,
+                };
             },
         },
     },
