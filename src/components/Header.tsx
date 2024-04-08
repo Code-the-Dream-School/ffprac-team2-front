03a945719ffed3de6ba1c30c61752121c5bce89c
mynewyear <natalia.iakovleva@hotmail.com>
2024-04-08 12:38:41 -0700

    prettier

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Flex, Image, Text } from '@chakra-ui/react';
-import Navigation from './Navigation'; 
+import Navigation from './Navigation';
 import logo from '../assets/logo.png';
 
 const Header: React.FC = () => {
-  return (
-    <Flex as="header" align="center" justify="space-between" wrap="wrap" padding="4" position="sticky" top="0" zIndex="banner">
-      <NavLink to="/">
-        <Flex align="center" gap={{ base: "3rem", sm: '2rem' }}>
-          <Image src={logo} alt="GetYourTutor" boxSize={{ base: '60px', lg: '80px' }} />
-          <Text fontSize={{ base: '29px', sm: '25g' }}  ml={2}>GetYourTutor</Text>
+    return (
+        <Flex
+            as="header"
+            align="center"
+            justify="space-between"
+            wrap="wrap"
+            padding="4"
+            position="sticky"
+            top="0"
+            zIndex="banner"
+        >
+            <NavLink to="/">
+                <Flex align="center" ml={5} gap={{ base: '4rem', sm: '2rem' }}>
+                    <Image src={logo} alt="GetYourTutor" boxSize={{ base: '60px', lg: '80px' }} />
+                    <Text
+                        fontSize={{ base: '29px', sm: '25g' }}
+                        fontFamily="'Inter', sans-serif"
+                        ml={2}
+                    >
+                        GetYourTutor
+                    </Text>
+                </Flex>
+            </NavLink>
+
+            {/* Navigation Links - depend on tutor/parent role and login logic */}
+            <Navigation />
         </Flex>
-      </NavLink>
-
-      {/* Navigation Links - tutor/parent role and login logic */}
-      <Navigation />
-    </Flex>
-  );
-
+    );
 };
 
 export default Header;
