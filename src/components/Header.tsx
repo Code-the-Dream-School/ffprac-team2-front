f51abf3e6b08c15ca23846a2309eee2c9d85ed14
mynewyear <natalia.iakovleva@hotmail.com>
2024-04-08 12:06:20 -0700

    add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Flex, Image, Text } from '@chakra-ui/react';
+import Navigation from './Navigation'; 
+import logo from '../assets/logo.png';
 
 const Header: React.FC = () => {
-    return (
-        <>
-            <h1>Header</h1>
-            <NavLink to={'/tutorsearch'}>to tutor search</NavLink>
-        </>
-    );
+  return (
+    <Flex as="header" align="center" justify="space-between" wrap="wrap" padding="4" position="sticky" top="0" zIndex="banner">
+      <NavLink to="/">
+        <Flex align="center" gap={{ base: "3rem", sm: '2rem' }}>
+          <Image src={logo} alt="GetYourTutor" boxSize={{ base: '60px', lg: '80px' }} />
+          <Text fontSize={{ base: '29px', sm: '25g' }}  ml={2}>GetYourTutor</Text>
+        </Flex>
+      </NavLink>
+
+      {/* Navigation Links - tutor/parent role and login logic */}
+      <Navigation />
+    </Flex>
+  );
+
 };
 
 export default Header;
