c47a7410d78a7a8a2c79a0c0048657962fd531ea
oxangyal <Oxana.mitch@gmail.com>
2024-04-21 00:11:10 -0400

    Change margin, push msg deeper

--- src/pages/TutorDashboard.tsx
+++ src/pages/TutorDashboard.tsx
@@ -92,7 +92,7 @@ const TutorDashboard: React.FC = () => {
                     ) : (
                         <>
                             {students.length === 0 ? (
-                                <Box textAlign="center" mt="50px" fontSize="18px">
+                                <Box textAlign="center" mt="100px" fontSize="16px">
                                     No students found. Please make sure your profile is fully
                                     complete to be appeared in search results and enable parents to
                                     connect with you.
